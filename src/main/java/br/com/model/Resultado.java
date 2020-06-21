cc675ebb3625b47494a4b00591b34514bc2aa55a
rafael.pinheiro <rafael.pinheiro@iviatecnologia.com.br>
2020-06-21 18:41:46 -0300

    provando tautologia

--- src/main/java/br/com/model/Resultado.java
+++ src/main/java/br/com/model/Resultado.java
@@ -1,12 +1,20 @@
 package br.com.model;
 
+import java.util.List;
+
 public class Resultado {
   private String expressao;
   private Boolean check;
+  private Boolean fechado;
+  private List<Resultado> resultados;
 
-  public Resultado(String expressao, Boolean check) {
+  public Resultado(String expressao) {
     this.expressao = expressao;
-    this.check = check;
+    this.check = false;
+  }
+
+  public Resultado(List<Resultado> resultados) {
+    this.resultados = resultados;
   }
 
   public String getExpressao() {
@@ -23,5 +31,21 @@ public class Resultado {
 
   public void setCheck(Boolean check) {
     this.check = check;
+  }
+
+  public List<Resultado> getResultados() {
+    return resultados;
+  }
+
+  public void setResultados(List<Resultado> resultados) {
+    this.resultados = resultados;
+  }
+
+  public Boolean getFechado() {
+    return fechado;
+  }
+
+  public void setFechado(Boolean fechado) {
+    this.fechado = fechado;
   }
 }
--- src/main/java/br/com/service/ExpressaoService.java
+++ src/main/java/br/com/service/ExpressaoService.java
@@ -11,24 +11,39 @@ import java.util.List;
 @Service
 public class ExpressaoService {
   List<Resultado> resultados = new ArrayList<>();
+  List<String> variaveis = new ArrayList<>();
 
   public void processaExpressao(ExpressaoLogica expressaoLogica) {
-    resultados.add(new Resultado(expressaoLogica.getExpressao(), false));
-    processador();
+    resultados.add(new Resultado(expressaoLogica.getExpressao()));
+    processador(resultados);
+    provaTautologia(resultados);
   }
 
-  private void processador() {
+  private void processador(List<Resultado> resultados) {
     String expressao = "";
-    for (Resultado resultado : resultados) {
+    Resultado resultado = null;
+    for (int i = 0; i < resultados.size(); i++) {
+      resultado = resultados.get(i);
       if (!resultado.getCheck()) {
         expressao = resultado.getExpressao();
+        resultado.setCheck(true);
         break;
       }
     }
-    List<String> lista = processaString(expressao);
-    lista = processaLista(lista);
-    verificaOrdemPrecedencia(lista, expressao);
-    processador();
+
+    if (expressao.length() != 2) {
+      List<String> lista = processaString(expressao);
+      lista = processaLista(lista);
+      verificaOrdemPrecedencia(lista, expressao);
+    }
+
+    if (resultado.equals(resultados.get(resultados.size() - 1))) {
+      if (resultado.getResultados() != null) {
+        processador(resultado.getResultados());
+      }
+    } else {
+      processador(resultados);
+    }
   }
 
   private List<String> processaString(String expressao) {
@@ -47,28 +62,6 @@ public class ExpressaoService {
         aux = aux + c;
       } else {
         lista.add(c.toString());
-      }
-    }
-    return lista;
-  }
-
-
-  private List<String> processaStringAux(String expressao) {
-    String aux = "";
-    Boolean parenteses = false;
-    List<String> lista = new ArrayList<>();
-    for (int i = 0; i < expressao.length(); i++) {
-      Character c = expressao.charAt(i);
-      if (parenteses || c == 'V' || c == 'F') {
-        lista.add(c.toString());
-        parenteses = false;
-      } else {
-        aux = aux + c;
-      }
-      if (c == ')') {
-        lista.add(aux);
-        aux = "";
-        parenteses = true;
       }
     }
     return lista;
@@ -124,30 +117,87 @@ public class ExpressaoService {
 
   private void processaImplica(List<String> lista, String expressao) {
     if (lista.get(3).equals("V")) {
-
+      List<Resultado> aux = new ArrayList<>();
+      aux.add(new Resultado(lista.get(0) + "F"));
+      aux.add(new Resultado(lista.get(2) + "V"));
+      adiciona(aux, null);
     } else {
-      this.resultados.add(new Resultado(lista.get(0) + "V", false));
-      this.resultados.add(new Resultado(lista.get(2) + "F", false));
-    }
-
-    for (Resultado resultado : resultados) {
-      if (resultado.getExpressao().equals(expressao)) {
-        resultado.setCheck(true);
-      }
+      adiciona(null, new Resultado(lista.get(0) + "V"));
+      adiciona(null, new Resultado(lista.get(2) + "F"));
     }
   }
 
   private void processaE(List<String> lista, String expressao) {
     if (lista.get(3).equals("V")) {
-      this.resultados.add(new Resultado(lista.get(0) + "V", false));
-      this.resultados.add(new Resultado(lista.get(2) + "V", false));
+      adiciona(null, new Resultado(lista.get(0) + "V"));
+      adiciona(null, new Resultado(lista.get(2) + "V"));
     } else {
-
+      List<Resultado> aux = new ArrayList<>();
+      aux.add(new Resultado(lista.get(0) + "F"));
+      aux.add(new Resultado(lista.get(2) + "V"));
+      adiciona(aux, null);
     }
+  }
 
-    for (Resultado resultado : resultados) {
-      if (resultado.getExpressao().equals(expressao)) {
+  /*private void setCheck(String expr, List<Resultado> result) {
+    for (Resultado resultado : result) {
+      if (resultado.getExpressao().equals(expr) && !resultado.getCheck()) {
         resultado.setCheck(true);
+      }
+    }
+  }*/
+
+//  private void setCheck2(String expr, List<Resultado> result) {
+//    for (Resultado resultado : result) {
+//      if (resultado.getExpressao() == null && resultado.getResultados() != null) {
+//        setCheck(expr, resultado.getResultados());
+//      } else if (resultado.getExpressao().equals(expr) && !resultado.getCheck()) {
+//        resultado.setCheck(true);
+//      }
+//    }
+//  }
+
+  private void adiciona(List<Resultado> lista, Resultado resultado) {
+    if (lista == null) {
+      resultados.add(resultado);
+    } else {
+      resultado = resultados.get(resultados.size() - 1);
+      if (resultado != null && resultado.getResultados() == null) {
+        resultado.setResultados(lista);
+      } else {
+        for (Resultado aux : resultado.getResultados()) {
+          aux.setResultados(lista);
+        }
+      }
+    }
+  }
+
+  private void provaTautologia(List<Resultado> resultados) {
+    for (Resultado resultado : resultados) {
+      String expressao = resultado.getExpressao();
+      if (expressao.length() == 2) {
+        variaveis.add(expressao);
+      }
+      if (resultado.getResultados() != null) {
+        provaTautologia(resultado.getResultados());
+      } else {
+        System.out.println();
+      }
+    }
+  }
+
+  private void provaTautologia2(List<Resultado> resultados) {
+    for (Resultado resultado : resultados) {
+      String expressao = resultado.getExpressao();
+      if (expressao.length() == 2) {
+        variaveis.add(expressao);
+      }
+      if (resultado.equals(resultados.get(resultados.size() - 1))) {
+        if (resultado.getResultados() != null) {
+          provaTautologia(resultado.getResultados());
+        } else {
+          System.out.println();
+        }
       }
     }
   }
